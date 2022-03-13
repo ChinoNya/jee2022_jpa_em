528f84ecf5196d117c4b854a4e1ec22060869ef0
0210彭勇杰 <cncapyj@gmail.com>
2022-03-13 21:18:58 +0800

    完成工程初始化

---
+++ src/main/java/cn/jee/jee2022_jpa_em/Jee2022JpaEmApplication.java
@@ -0,0 +1,13 @@
+package cn.jee.jee2022_jpa_em;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jee2022JpaEmApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Jee2022JpaEmApplication.class, args);
+  }
+
+}
---
+++ src/test/java/cn/jee/jee2022_jpa_em/Jee2022JpaEmApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jee.jee2022_jpa_em;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jee2022JpaEmApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
